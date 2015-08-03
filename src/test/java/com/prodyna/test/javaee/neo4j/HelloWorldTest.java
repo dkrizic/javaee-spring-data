9db779d213e5b66156cafd977aad34e1dd809664
Darko Krizic <dkrizic@macpro.prodyna.com>
2015-08-03 15:07:19 +0200

    Basically the access to a local running Neo4j works using a RestGraphDatabase.

--- src/test/java/com/prodyna/test/javaee/neo4j/HelloWorldTest.java
+++ src/test/java/com/prodyna/test/javaee/neo4j/HelloWorldTest.java
@@ -4,6 +4,7 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,12 +18,15 @@ public class HelloWorldTest {
     @Deployment
     public static WebArchive deployment() {
         WebArchive war = create(WebArchive.class, "hello.war");
-        war.addPackages( true, "com.prodyna.test.javaee.neo4j");
+        war.addPackages(true, "com.prodyna.test.javaee.neo4j");
         war.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        war.addAsLibraries(Maven.resolver().loadPomFromFile("pom.xml").importRuntimeDependencies().resolve().withTransitivity().asFile());
+        System.out.println(war.toString(true));
         return war;
     }
 
-    @Inject private HelloWorld hello;
+    @Inject
+    private HelloWorld hello;
 
     @Test
     public void dummy() {
